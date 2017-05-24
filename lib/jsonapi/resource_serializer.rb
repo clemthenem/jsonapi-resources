0a833bf6aa608ae47661f109d368765ddb9a0e06
Clement <clementberti@yahoo.fr>
2017-05-24 16:18:25 +1000

    quick bug fix

--- lib/jsonapi/resource_serializer.rb
+++ lib/jsonapi/resource_serializer.rb
@@ -459,7 +459,11 @@ module JSONAPI
         # If you have direct access to the underlying id, you don't have to load the relationship
         # which can save quite a lot of time when loading a lot of data.
         # This does not apply to e.g. has_one :through relationships.
-        source.public_send("#{relationship.name}_id")
+        begin
+          source.public_send("#{relationship.name}_id")
+        rescue
+          source.public_send(relationship.name).try(:id)
+        end
       else
         source.public_send(relationship.name).try(:id)
       end
